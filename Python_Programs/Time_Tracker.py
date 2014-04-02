735a722049fdb13e8fd015b635c53cf0dbd6485d
Michael Arnold <valestrum@hotmail.com>
2014-04-01 22:24:16 -0400

    Created a program to track run time of programs.

---
+++ Python_Programs/Time_Tracker.py
@@ -0,0 +1,10 @@
+# This program is a timer to see how long a program takes to finish executing.
+
+import time
+
+def main():
+    start = time.time()
+    # Insert code here
+    elapsed = time.time() - start
+    print 'elapsed time: %3f ms' % int(round(elapsed* 1000))
+main()
