82bc0bcdd9389aaf5b2d58f7f7104c8018c6aae9
Michael Arnold <valestrum@hotmail.com>
2014-04-22 23:32:10 -0400

    Create Tax_Calc.py

---
+++ Mega_Project_List_Solutions/Tax_Calc.py
@@ -0,0 +1,7 @@
+def main():
+    cost = input("Enter cost of item: ")
+    total = (cost * 1.06) # The tax rate where I live is 6%
+    print("The total price with tax is: " + str(total))
+main()
+
+#Note to self: I should probably make this fancier later and add more options.
