b6877cb1f3aa16a2cb07241e24e26567a6c37ac5
Michael Arnold <valestrum@hotmail.com>
2014-04-24 05:12:12 -0400

    Created "Coin Flip Simulator" in Python

---
+++ Mega_Project_List_Solutions/Coin_Flip.py
@@ -0,0 +1,19 @@
+import random
+
+def main():
+    heads = tails = count = 0 #Note: Use a,b,c = 0,1,2 format if other values.
+
+    flips = input("How many times would you like to flip a coin? ")
+    while(count < flips):
+        result = random.randint(0,1)
+        if (result == 0):
+            heads = heads + 1
+        elif (result == 1):
+            tails = tails + 1
+        else:
+            print("Program error.")
+        count = count + 1
+    print("Times landed on heads: " + str(heads))
+    print("Times landed on tails: " + str(tails))
+
+main()
