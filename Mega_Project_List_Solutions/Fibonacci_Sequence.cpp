619e1d10fa82b5cff86e15738492d95acedd7e95
Michael Arnold <valestrum@hotmail.com>
2014-06-12 21:04:39 -0400

    Cleaned code to remove unneeded dependencies.

--- Mega_Project_List_Solutions/Fibonacci_Sequence.cpp
+++ Mega_Project_List_Solutions/Fibonacci_Sequence.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h" // For Visual Studio
 #include <iostream>
 using namespace std;
 
@@ -6,19 +5,17 @@ int main()
 {
 	int a = 0, b = 1, c = 0;
 	int fib_num = 0, count = 0;
-	cout << "Enter the place of the fibonacci number you want to find: ";
-	cin >> fib_num;
+	std::cout << "Enter the place of the fibonacci number you want to find: ";
+	std::cin >> fib_num;
 
 	while (count < fib_num)
 	{
 		count++;
 		c = a + b;
-		cout << c << endl; // Remove this line to only see final number.
+		std::cout << c << std::endl; // Remove this line to only see final number.
 		a = b;
 		b = c;
 	}
-	cout << "The value of the " << fib_num << " place in the Fibonacci Sequence is: " << c << "\n\n";
-
-	system("pause");
+	std::cout << "The value of the " << fib_num << " place in the Fibonacci Sequence is: " << c << "\n\n";
 	return 0;
 }
