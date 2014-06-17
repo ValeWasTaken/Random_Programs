7312b9fed3927fd52ce0f5b6ad882504acc22021
Michael Arnold <valestrum@hotmail.com>
2014-06-16 22:42:10 -0400

    Update and rename Mega_Project_List_Solutions/Reverse_String.py to Mega_Project_List_Solutions/Text/Reverse_String.py

--- Mega_Project_List_Solutions/Text/Reverse_String.py
+++ Mega_Project_List_Solutions/Text/Reverse_String.py
@@ -1,11 +1,9 @@
 def main():
     original_string = raw_input("Enter a string to be reversed: ")
     reversed_string = ''
-
     i = len(original_string) - 1
     while(i >= 0):
         reversed_string += original_string[i]
         i -= 1
-
     print(reversed_string)
 main()
