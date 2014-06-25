d8596f889e542425e20f3f105a33d60d8fdb0cfb
Michael Arnold <valestrum@hotmail.com>
2014-06-24 21:26:55 -0400

    Create starcraft2_GDforum_average_postCount.py

---
+++ Python_Programs/starcraft2_GDforum_average_postCount.py
@@ -0,0 +1,22 @@
+import urllib
+from bs4 import BeautifulSoup
+import time
+import os
+
+# Finds the average amount of replies (per thread) on the first page of the Starcraft II General Discussion Forums.
+
+def readLog():
+        url = 'http://us.battle.net/sc2/en/forum/40568/'
+        htmlfile = urllib.urlopen(url)
+        htmltext = htmlfile.read()
+        soup = BeautifulSoup(htmltext)
+
+        sumOfReplies = 0
+        thread = 0
+        while(thread < 43):
+                replyCount = soup.findAll('td', {'class':'reply-cell'})[thread].contents[0]
+                thread += 1
+                sumOfReplies += int(replyCount)
+        replyAverage = (int(sumOfReplies) / 44.00) # Delete '.00' to get a whole number.
+        print("The average reply count is: %s" % replyAverage)
+readLog()
