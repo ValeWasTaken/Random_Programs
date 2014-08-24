af9d30d0db8545acd525505b63553d3fe7c3ca8c
Michael Arnold <valestrum@hotmail.com>
2014-08-23 22:07:26 -0400

    Rewrote start and end, overall optimization.

--- Mega_Project_List_Solutions/Numbers/Fibonacci_Sequence.py
+++ Mega_Project_List_Solutions/Numbers/Fibonacci_Sequence.py
@@ -1,10 +1,5 @@
 def main():
-    a = 0
-    b = 1
-    c = 0
-    fib_num = 0
-    count = 0
-
+    a,b,c,fib_num,count = 0,1,0,0,0
     fib_num = input("Enter the place of the fibonacci number you want to find: ")
     while (count != fib_num):
         count += 1
@@ -12,5 +7,5 @@ def main():
         print(c) # Remove this line to see only the final number.
         a = b
         b = c
-    print("Your selected number's place in the Fiboacci Sequence is: " + str(c))
+    print("Your requested sequence place, %d, in the Fiboacci Sequence is: %s" % (fib_num,c))
 main()
