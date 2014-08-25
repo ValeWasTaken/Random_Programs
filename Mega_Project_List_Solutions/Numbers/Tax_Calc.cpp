ea2521aa0e4a658d09d6d090383e5ab25fe7f311
Michael Arnold <valestrum@hotmail.com>
2014-08-24 20:42:30 -0400

    Removed dependency

--- Mega_Project_List_Solutions/Numbers/Tax_Calc.cpp
+++ Mega_Project_List_Solutions/Numbers/Tax_Calc.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
     double cost = 0;
-    cout << "Enter cost of item: ";
-    cin >> cost;
+    std::cout << "Enter cost of item: ";
+    std::cin >> cost;
     double total = (cost * 1.06); // The current tax rate here is 6%.
-    cout << "The total price with tax is: $" << total << "\n\n";
+    std::cout << "The total price with tax is: $" << total << "\n\n";
     return 0;
 }
