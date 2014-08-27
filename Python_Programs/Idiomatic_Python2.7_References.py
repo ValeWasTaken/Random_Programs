2270426be2c3814c604bc157e060f3a0fa031865
Michael Arnold <valestrum@hotmail.com>
2014-08-27 19:46:59 -0400

    Added 8th Idiotmatic Example.

--- Python_Programs/Idiomatic_Python2.7_References.py
+++ Python_Programs/Idiomatic_Python2.7_References.py
@@ -142,3 +142,28 @@ def print_table(operator):
             
 for operator in (op.add, op.sub, op.mul, op.div):
     print_table(operator)
+
+
+
+# Example 8: Use Exceptions to Write Code in "EAFP" Style (Easier to ask forgiveness than for permission)
+# -- Harmful --
+def get_log_level(config_dict):
+    if 'ENABLE_LOGGING' in config_dict:
+        if config_dict['ENABLE_LOGGING'] != True:
+            return None
+        elif not 'DEFAULT_LOG_LEVEL' in config_dict:
+            return None
+        else:
+            return config_dict['DEFAULT_LOG_LEVEL']
+    else:
+        return None
+
+# -- Idiomatic --
+def get_log_level(config_dict):
+    try:
+        if config_dict['ENABLE_LOGGING']:
+            return config_dict['DEFAULT_LOG_LEVEL']
+    except KeyError:
+    # if either value wasn't present,
+    # a KeyError will be raised, so return None
+    return None
