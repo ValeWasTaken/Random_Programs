8f385a2aa73673eae20f2df469ca05bd9c61cc3b
Michael Arnold <valestrum@hotmail.com>
2014-08-29 19:02:09 -0400

    Added a 10th example

--- Python_Programs/Idiomatic_Python2.7_References.py
+++ Python_Programs/Idiomatic_Python2.7_References.py
@@ -190,3 +190,26 @@ hash_value = 0
 for e in some_string:
     hash_value += ord(e)
 return hash_value
+
+
+
+# Example 10: Prefer the format function for formatting strings
+# -- Harmful --
+def get_formatted_user_info_worst(user):
+    # Tedious to type and prone to conversion errors
+    return 'Name: ' + user.name + ', Age: ' + \
+        str(user.age) + ', Sex: ' + user.sex
+
+def get_formatted_user_info_slightly_better(user):
+    # No visible connection between the format string placeholders
+    # and values to use. Also, why do I have to know the type?
+    # Don't these types all have __str__ functions?
+    return 'Name: %s, Age: %i, Sex: %c' % (user.name, user.age, user.sex)
+
+# -- Idiotmatic --
+def get_formatted_user_info(user):
+    # Clear and concise. At a glance I can tell exactly what
+    # the output should be. Note: this string could be returned
+    # directly, but the string itself is too long to fit on the page.
+    output = 'Name: {user.name}, Age: {user.age}, Sex: {user.sex}'.format(user=user)
+    return output
