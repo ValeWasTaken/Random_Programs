49a816e6edc1991a7e83384c9d94ad50cea45fbc
Michael Arnold <valestrum@hotmail.com>
2014-08-31 20:47:58 -0400

    Added 14th example(document what it does, not how)

--- Python_Programs/Idiomatic_Python2.7_References.py
+++ Python_Programs/Idiomatic_Python2.7_References.py
@@ -246,3 +246,25 @@ for index in xrange(1000000):
     if index % 2 == 0:
         even_number = index
         break
+
+
+
+# Example 14: Document what something does, not how.
+# -- Harmful --
+def is_prime(number):
+        """Mod all numbers from 2 -> number and return True
+        if the value is never 0"""
+        for candidate in range(2, number):
+                if number % candidate == 0:
+                        print(candidate)
+                        print(number % candidate)
+                        return False
+        return number > 0
+
+# -- Idiotmatic --
+def is_prime(number):
+        """Return True if number is prime"""
+        for candidate in range(2, number):
+                if number % candidate == 0:
+                        return False
+        return number > 0
