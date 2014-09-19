d2f2cfc0cc3a83a14fc8ad58fbb0270fb902a93f
Michael Arnold <valestrum@hotmail.com>
2014-09-18 21:05:12 -0400

    Optimized program / reduced lines

--- Mega_Project_List_Solutions/Numbers/Tax_Calc.py
+++ Mega_Project_List_Solutions/Numbers/Tax_Calc.py
@@ -1,5 +1,4 @@
 def main():
     cost = input("Enter cost of item: ")
-    total = (cost * 1.06)
-    print("The total price with tax is: " + str(total))
+    print("The total price with tax is: " + str(cost * 1.06))
 main()
