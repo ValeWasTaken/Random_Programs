1eb1df0339f6ce902b4547ce76eaafe0e61ece32
Michael Arnold <valestrum@hotmail.com>
2015-02-02 19:42:30 -0500

    Update beerSong.java

--- Java/beerSong.java
+++ Java/beerSong.java
@@ -11,8 +11,7 @@ public class beerSong {
 			
 			System.out.println(beerNum + " " + word + " of beer on the wall");
 			System.out.println(beerNum + " " + word + " of beer");
-			System.out.println("Take one down");
-			System.out.println("Pass it around");
+			System.out.println("Take one down\nPass it around");
 			beerNum--;
 		}
 		if(beerNum == 0) {System.out.println("No more bottles of beer on the wall");}
