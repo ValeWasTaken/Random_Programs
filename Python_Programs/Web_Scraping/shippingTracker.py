c451727bcb093ae4a4a8554b1eef1eada4b10293
Michael Arnold <valestrum@hotmail.com>
2015-02-12 23:01:31 -0500

    Slightly optimized program
    Added an array of tracking numbers so that I could use the same URL rather than having an array of URLs.

--- Python_Programs/Web_Scraping/shippingTracker.py
+++ Python_Programs/Web_Scraping/shippingTracker.py
@@ -4,24 +4,18 @@ import time
 import os
 
 def track():
-        urls = ["http://webtrack.dhlglobalmail.com/?id=2720&trackingnumber=<censoredForGitHub>",
-                "http://webtrack.dhlglobalmail.com/?id=2720&trackingnumber=<censoredForGitHub>",
-                "http://webtrack.dhlglobalmail.com/?id=2720&trackingnumber=<censoredForGitHub>",
-                "http://webtrack.dhlglobalmail.com/?id=2720&trackingnumber=<censoredForGitHub>",
-                "http://webtrack.dhlglobalmail.com/?id=2720&trackingnumber=<censoredForGitHub>",
-                "http://webtrack.dhlglobalmail.com/?id=2720&trackingnumber=<censoredForGitHub>",
-                "http://webtrack.dhlglobalmail.com/?id=2720&trackingnumber=<censoredForGitHub>",
-                "http://webtrack.dhlglobalmail.com/?id=2720&trackingnumber=<censoredForGitHub>",]
-        for x in range(len(urls)):
-                soup = BeautifulSoup(urllib.urlopen(urls[x]).read())
-                statusInfo = soup.find_all("div", class_="status-info")
-                for status in statusInfo:
-                        print("Shipment order number: "+str(x)+"\n" + status.get_text("\n", strip=True) + "\n")
+    tr = ['example1','ex2','ex3','ex4','ex5','ex6','ex7','ex8'] # tr = Tracking Number in URL
+    for x in range(len(tr)):
+        url = "http://webtrack.dhlglobalmail.com/?id=2720&trackingnumber="+str(tr)
+        soup = BeautifulSoup(urllib.urlopen(url).read())
+        statusInfo = soup.find_all("div", class_="status-info")
+        for status in statusInfo:
+            print("Shipment order number: "+str(x)+"\n" + status.get_text("\n", strip=True) + "\n")
 
 def main():
-        while True:
-                os.system('clear')
-                track()
-                time.sleep(3600) # 1 hour
+    while True:
+        os.system('clear')
+        track()
+        time.sleep(3600) # 1 hour
 main()
 # Screenshot of running program: https://i.imgur.com/AHiLyNw.png
