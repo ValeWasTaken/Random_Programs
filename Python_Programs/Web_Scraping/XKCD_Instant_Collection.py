22dc6dd15732ab405c0e2d466e4a7e7f76afd950
Michael Arnold <valestrum@hotmail.com>
2015-05-05 22:50:25 -0400

    Create XKCD_Instant_Collection.py

---
+++ Python_Programs/Web_Scraping/XKCD_Instant_Collection.py
@@ -0,0 +1,34 @@
+# Python 2.7.9 - XKCD Comic Scraper
+# Downloads and saves all XKCD comics to your computer. 
+# Make sure you run this within your desired XKCD picture folder!
+
+import urllib
+import re
+import requests
+from bs4 import BeautifulSoup
+
+def scrapeXKCD():
+    website = requests.get("http://xkcd.com/").content
+    newestComicData = re.compile(r'Permanent link to this comic: http://xkcd.com/(\d+)/')
+    newestComicNumber = int(newestComicData.search(website).group(1))
+
+    soup = BeautifulSoup(urllib.urlopen('http://xkcd.com/700/').read())
+    title = soup.find("div", id="comic").img['alt']
+    image = soup.find("div", id="comic").img['src']
+
+    for comic in range(1,newestComicNumber+1):
+        if comic == 404:
+            pass # 404 returns a 404 error. (Very funny, XKCD. Very funny.)
+        else:
+            try:
+                soup = BeautifulSoup(urllib.urlopen('http://xkcd.com/%s/' % comic).read())
+                title = soup.find("div", id="comic").img['alt']
+                title = ''.join(char for char in title if char.isalnum() or char == ' ') # Gets rid of special characters for filename
+                image = "http:"+str(soup.find("div", id="comic").img['src'])
+                while len(str(comic)) < len(str(newestComicNumber)):
+                    comic = '0'+str(comic) # So that "1" becomes "0001" meaning your files don't go "1,10,11,..,2,20,.."
+                fileName = "XKCD_" + str(comic) + "_" + title + ".jpg"
+                urllib.urlretrieve(image,fileName)
+            except TypeError:
+                pass # Some comics like #1350 and #1416 are not pictures and are interactable.
+scrapeXKCD()
