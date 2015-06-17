2e3b642972188f8e76d408e534da2e97efd213e1
Michael Arnold <valestrum@hotmail.com>
2015-06-16 21:46:33 -0400

    Create Hello_World.py

---
+++ Python_Programs/Kivy/Android/Hello_World.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class Hello(App):
+    def build(self):
+        btn = Button(text='Hello World!')
+        return  btn
+
+Hello().run()
