83607fd4bd622046fa7e12ed6ceb2008b950efab
Michael Arnold <valestrum@hotmail.com>
2015-08-02 23:13:57 -0400

    Fixed syntax typo in .format()

--- Mega_Project_List_Solutions/Numbers/Fibonacci_Sequence.py
+++ Mega_Project_List_Solutions/Numbers/Fibonacci_Sequence.py
@@ -1,8 +1,8 @@
 a,b,c = 0,1,0
 place = int(input("Enter the place of the Fibonacci number you want to find: "))
 for x in range(place):
-    #print(a) -- Current Fibonacci number.
+    #print(a) # -- Print the current Fibonacci number.
     c = a
     a += b
     b = c
-print("Your request sequence place, {0}, in the Fibonacci Sequence is: {1}").format(place,c)
+print("Your request sequence place, {0}, in the Fibonacci Sequence is: {1}".format(place,c))
