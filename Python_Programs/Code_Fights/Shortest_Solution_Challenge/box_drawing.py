de627d02247a4f0836371c42d54e40cf00c9dbda
Michael Arnold <valestrum@hotmail.com>
2015-08-03 22:13:35 -0400

    Solved a programming challenge

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/box_drawing.py
@@ -0,0 +1,29 @@
+'''
+Box drawing challenge in Python 3.4
+Required code: 
+      def box(n):
+Goal: Create a box that made out of *s with an empty middle based on user inputted number.
+Note: 0 must return '' and 1 must return '*'.
+Example:
+  >> 4
+  >> ****
+  >> *  *
+  >> *  *
+  >> ****
+Solve with as few characters as possible.
+
+Characters used in solution: 110
+'''
+
+def box(n):
+    a=''
+    s='*'
+    b=n-2
+    if n<2:
+        return{0:a,1:s}[n]
+    else:
+        a+=s*n+'\n'
+        for x in range(b):
+            a+=s+(b*' ')+s+'\n'
+        a+=s*n
+    return a
