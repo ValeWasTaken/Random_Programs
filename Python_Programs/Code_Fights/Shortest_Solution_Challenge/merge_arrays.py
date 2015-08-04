06a370c59686ae7346a51cf25d1f9b5acc1324dd
Michael Arnold <valestrum@hotmail.com>
2015-08-04 01:07:50 -0400

    Create merge_arrays.py

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/merge_arrays.py
@@ -0,0 +1,15 @@
+'''
+Merge arrays challebge in Python 3.4
+Required code: 
+      mergeArray
+Goal: Add the two arrays together. 
+Note: You must return your answer modulo by 10**9+7.
+Example:
+  >> a = ["A", "E", "I" ]
+  >> b = [ "O", "U" ]
+  
+  >> Output = [ "A", "E", "I" , "O", "U"]
+Solve with as few characters as possible.
+Characters used in solution: 24
+'''
+mergeArrays = list.__add__
