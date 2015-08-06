40d08992cbb94e07794cdbc72eebc1c2f7523ca4
Michael Arnold <valestrum@hotmail.com>
2015-08-05 22:21:40 -0400

    Create Sum_Of_Multiples_3_Or_5.py

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/Sum_Of_Multiples_3_Or_5.py
@@ -0,0 +1,12 @@
+'''
+Sum_Of_Multiples_3_Or_5 challenge in Python 3.4
+Required code: 
+      def Sum_Of_Multiples_3_Or_5(N):
+Goal: Find the sum of all numbers divisable by 3 and/or 5 under N.
+Example:
+  >> N = 10 -> 3 + 5 + 6 + 9 = 23
+Solve with as few characters as possible.
+Characters used in solution: 74
+'''
+def Sum_Of_Multiples_3_Or_5(N):
+    return sum([x for x in range(N) if x % 3 == 0 or x % 5 == 0])
