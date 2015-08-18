bd0f5c0711d719fb6392cfc55f7292c313c70fdb
Michael Arnold <valestrum@hotmail.com>
2015-08-17 22:27:02 -0400

    Updated for Python 3.4

--- Mega_Project_List_Solutions/Numbers/Coin_Flip.py
+++ Mega_Project_List_Solutions/Numbers/Coin_Flip.py
@@ -1,17 +1,15 @@
+# Python 3.4
 import random
 
-def main():
-    heads = tails = count = 0 #Note: Use a,b,c = 0,1,2 format if other values.
-    flips = input("How many times would you like to flip a coin? ")
+def coin_flip():
+    heads, tails, count = 0, 0, 0
+    flips = int(input("Number of flips: "))
     while(count < flips):
         result = random.randint(0,1)
-        if (result == 0):
+        if result == 0:
             heads += 1
-        elif (result == 1):
-            tails += 1
         else:
-            print("Program error.")
+            tails += 1
         count += 1
-    print("Times landed on heads: "+str(heads))
-    print("Times landed on tails: "+str(tails))
-main()
+    print("Results: {0} landed on heads. {1} landed on tails.".format(heads, tails))
+coin_flip()
