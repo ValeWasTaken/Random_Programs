27f12982331275a61e71f07d0d706749b2439289
Michael Arnold <valestrum@hotmail.com>
2015-09-06 20:29:13 -0400

    Solved a new CodeFight problem

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/virus_scan.py
@@ -0,0 +1,12 @@
+'''
+virus_scan challenge in Python 3.4
+Required code: 
+      def virusScan(input):
+Goal: Detect if the string 'virus' is contained within the given string, regardless of letter case.
+Example:
+  >> 'thisIsAviRuS
+  >> True
+Solve with as few characters as possible.
+Characters used in solution: 36
+'''
+virusScan = lambda n: 'virus' in n.lower()
