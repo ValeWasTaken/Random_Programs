d627ab10f4ad5cbbc5c9bc6581df2fd83c268091
Michael Arnold <valestrum@hotmail.com>
2015-09-20 23:08:24 -0400

    Solved bear_pyramid challenge in Python 3.4

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/bear_pyramid.py
@@ -0,0 +1,26 @@
+'''
+bear_pyramid challenge in Python 3.4
+Required code: 
+      def bear_pyramid(N):
+Goal: Fight the height of the largest teddy bear pyramid Justin can make.
+Note: Teddy bear height is 3 + 1 for the head at the top of the pyramid.
+Example:
+  >> 6
+  >>   * 3 + 1
+  >>  * * + 3
+  >> * * * + 3
+  >> Height = 10
+Solve with as few characters as possible.
+Characters used in solution: 71
+'''
+def bear_pyramid(N):
+    c = m = 0
+    h = 1
+    for x in range(N+1):
+        if c > m:
+            m = c
+            c = 0
+            h += 3
+        c += 1
+    return h
+print(bear_pyramid(7))
