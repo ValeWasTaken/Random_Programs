e57efda5d91db2c8c7200b973556fa9912544436
Michael Arnold <valestrum@hotmail.com>
2015-10-04 20:51:56 -0400

    Solved a new challenge using Python 3.4

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/sum_occur.py
@@ -0,0 +1,13 @@
+'''
+sum_occur challenge in Python 3.4
+Required code: 
+      def sumOccur(A):
+Goal: Find the sum of all numbers in the array.
+Example:
+  >> [2 , 2 , 2 , 4 , 4 , 5]
+  >> (2*3) + (4*2) + (5*1)
+  >>  19
+Solve with as few characters as possible.
+Characters used in solution: 23
+'''
+sumOccur = lambda A: sum(A)
