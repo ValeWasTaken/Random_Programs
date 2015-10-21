def10554371c3236f355757db08ada85ac6f3d8e
Michael Arnold <valestrum@hotmail.com>
2015-10-20 20:43:10 -0400

    Added new solution, dominant_species.py

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/dominant_species.py
@@ -0,0 +1,12 @@
+'''
+dominant_species challenge in Python 3.4
+Required code: 
+      def dominant_species(A):
+Goal: Find the most common number in a list.
+Example:
+  >> [1,2,1,3,3,1,0,1,1,9,1]
+  >> 1
+Solve with as few characters as possible.
+Characters used in solution: 48
+'''
+Dominant_species = lambda A: max(set(A), key=A.count)
