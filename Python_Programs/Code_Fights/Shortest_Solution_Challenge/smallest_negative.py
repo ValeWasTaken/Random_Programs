6a52cb6698df671c96dd810fce52f5a93b2347fd
Michael Arnold <valestrum@hotmail.com>
2015-11-06 23:03:12 -0500

    Solved new CodeFight challenge.

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/smallest_negative.py
@@ -0,0 +1,13 @@
+'''
+smallest_negative challenge in Python 3.4
+Required code: 
+      def smallestNegative(A):
+Goal: Given an array of numbers find the smallest negative number. If there are no negatives return "NOT_FOUND"
+Example:
+  >> smallestNegative([1,2,3]
+  >> "NOT_FOUND"
+  >> smallestNegative([5,-2,-3]
+  >> "-3"
+Solve with as few characters as possible.
+Characters used in solution: 61
+'''
