663eda8632035995533ccce744c43d937cf4697c
Michael Arnold <valestrum@hotmail.com>
2015-12-12 23:54:23 -0500

    Slightly optimized time counter

--- Python_Programs/Time_Tracker.py
+++ Python_Programs/Time_Tracker.py
@@ -4,5 +4,5 @@ def main():
     start = time.time()
     # Insert code here
     elapsed = time.time() - start
-    print('elapsed time: %3f ms' % int(round(elapsed*1000)))
+    print('Elapsed time: %.3f ms' % float(elapsed*1000))
 main()
