fefc787fc40bc16c7dafa1d0aaef4bda291345ef
Michael Arnold <valestrum@hotmail.com>
2016-01-28 23:12:06 -0500

    Optimized solution.

--- Mega_Project_List_Solutions/Numbers/Tax_Calc.py
+++ Mega_Project_List_Solutions/Numbers/Tax_Calc.py
@@ -1,4 +1,2 @@
-def main():
-    cost = input("Enter cost of item: ")
-    print("The total price with tax is: " + str(cost * 1.06))
-main()
+# Python 3.4
+with_tax = lambda cost: cost * 1.07 # Assuming tax rate = 7%
