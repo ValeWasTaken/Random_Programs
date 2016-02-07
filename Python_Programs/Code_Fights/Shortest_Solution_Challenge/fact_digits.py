d07b4f282817d6f3b1d975fef58dfdc1f86dab79
Michael Arnold <valestrum@hotmail.com>
2016-02-06 22:27:30 -0500

    Create fact_digits.py

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/fact_digits.py
@@ -0,0 +1,22 @@
+'''
+fact_digits challenge in Python 3.4
+Required code: 
+      def fact_digits(n):
+Goal: Return the length of the sum of the factorial number.
+Example:
+  >> fact_digits(10) = 7
+  >> 10! = 10*9*8*7*6*5*4*3*2*1 = 3628800 = 7 digits in length.
+Solve with as few characters as possible.
+Characters used in solution: 57 (using math.factorial)
+'''
+
+import math
+fact_digits = lambda n: len(str(math.factorial(n)))
+
+
+'''
+Or if solved the long way:
+
+def fact_digits(n):
+    return len(str(int(math.sqrt(2 * math.pi * n) * (n / math.e) ** n)))
+'''
