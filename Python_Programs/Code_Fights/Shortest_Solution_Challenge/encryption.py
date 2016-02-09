0c8c97f2ff1fc9c4acd2f6977d1fc4359fca33d5
Michael Arnold <valestrum@hotmail.com>
2016-02-08 23:44:40 -0500

    Created solution to new CodeFight

---
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/encryption.py
@@ -0,0 +1,24 @@
+'''
+Encryption challenge in Python 3.4
+Required code: 
+      def Encryption(Message, k):
+Goal: "Encrypt" the message with a ceasar cipher.
+Example:
+  >> Encryption("Ab", 1) = "Bc"
+  >> Encryption("z", 2) = "b"
+Solve with as few characters as possible.
+Characters used in solution: 120 : Work-in-progress
+'''
+
+def Encryption(m, k):
+    e = []
+    a = []
+    for c in m[::]:
+        e.append(ord(c))
+    for x in e:
+        x = int(x)
+        x += k
+        if x > 122:
+            x -= 26
+        a.append(chr(x))
+    return ''.join(a)
