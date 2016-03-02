1df1c0303dfd3e8d2345c9022023ae2c675648d8
Michael Arnold <valestrum@hotmail.com>
2016-03-01 22:50:23 -0500

    Create annoying_post.py

---
+++ Python_Programs/Misc/annoying_post.py
@@ -0,0 +1,18 @@
+# Python 3.4
+# Using Comic Sans font, make a post multi-colored to mess with other users. (I'm so sorry..)
+import random
+
+post = input()
+
+def annoying_post(post):
+    annoying_post = []
+    
+    for word in post.split():
+        color = "[color=#%06x]" % random.randint(0, 0xFFFFFF)
+        annoying_post.append("{0}{1}[/color]".format(color, word))
+
+    words = ' '.join(annoying_post)
+    result = "[font=Comics Sans MS]{0}[/font]".format(words)
+    print(result)
+    
+annoying_post(post)
