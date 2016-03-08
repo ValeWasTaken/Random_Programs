b34f26530e5a1f60398257b62fbb854a1cb50057
Michael Arnold <valestrum@hotmail.com>
2016-03-08 00:06:50 -0500

    Reduced to 158 characters

--- Python_Programs/Code_Fights/Shortest_Solution_Challenge/replace_words.py
+++ Python_Programs/Code_Fights/Shortest_Solution_Challenge/replace_words.py
@@ -9,16 +9,14 @@ Example:
   >> sentence: "I am the best"
   >> "Best the am I"
 Solve with as few characters as possible.
-Characters used in solution: 164
+Characters used in solution: 158
 '''
 
 def replaceWords(s):
     s = s.lower().split()
     o = sorted(s, key=len)
-    r = o[::-1]
     a = []
     for w in s:
-        a.append(r[o.index(w)])
-    a = ['I' if i=='i' else i for i in a]
-    a = ' '.join(a)
+        a.append(o[::-1][o.index(w)])
+    a = ' '.join(['I' if i=='i' else i for i in a])
     return a[0].upper() + a[1:]
